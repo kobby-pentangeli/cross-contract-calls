26af9993916e4878780e818f67cf931efe5d126d
Kobby Pentangeli <kobbypentangeli@gmail.com>
2023-08-18 11:41:17 +0000

    Implement 3 simple smart contracts

--- src/contract_b.rs
+++ src/contract_b.rs
@@ -1,14 +1,8 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+extern "C" {
+    fn execute_contract_c() -> i32;
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[no_mangle]
+pub extern "C" fn execute_contract_b() -> i32 {
+    unsafe { execute_contract_c() }
 }
--- src/contract_c.rs
+++ src/contract_c.rs
@@ -1,14 +1,5 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[no_mangle]
+pub extern "C" fn execute_contract_c() -> i32 {
+    // The "Answer to the Ultimate Question of Life, the Universe, and Everything"
+    42
 }
